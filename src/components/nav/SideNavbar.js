6be08e985e4c5a44410b1eae22d3f438c7abe1a0
xrbarrantes11 <randy.barrantes.pizarro@est.una.ac.cr>
2025-01-20 14:25:38 -0600

    changes sideBar

--- src/components/nav/SideNavbar.js
+++ src/components/nav/SideNavbar.js
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
 import "./SideNavbar.css";
 
 export const SideNavbar = () => {
   const [activeNav, setActiveNav] = useState("#");
-
-  useEffect(() => {
-    const sections = document.querySelectorAll("section");
-    const observerOptions = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5, // Se activa cuando el 60% de la sección está visible
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveNav(`#${entry.target.id}`); // Actualiza el estado según la sección visible
-        }
-      });
-    }, observerOptions);
-
-    sections.forEach((section) => {
-      observer.observe(section); // Observa todas las secciones
-    });
-
-    return () => observer.disconnect(); // Limpia el observador al desmontar el componente
-  }, []);
 
   return (
     <nav>
